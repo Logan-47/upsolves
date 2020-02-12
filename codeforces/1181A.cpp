2758532d7a6a2fe5b8f90a7fde7def5736a9a1c9
Ashish Singh <ashishofficial47@gmail.com>
2020-02-12 23:37:59 +0530

    5

---
+++ codeforces/1181A.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+using ll = long long;
+#define whatis(x) cout << "> "<<#x << " ==> " << x << endl;
+#define br cout<< endl;
+#define vi vector<int>
+#define vii vector<ll>
+#define REP(i, k) for(int i=1; i<=k;i++)
+#define REPV(i, k) for(int i=0; i<k;i++)
+#define all(v) begin(v),end(v)
+#define minv(a) *min_element(all(a))
+#define maxv(a) *max_element(all(a))
+#define inputv(a) forall(i,0,a.size()) cin>>a[i]
+#define outputv(a) for(auto i:a) cout<<i<<" ";br;
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define print(x) cout << x << " ";
+#define outputvp(a) for(auto i:a) {for(auto j: i) {cout<<j.first<<" " << j.second << ", ";}br;}
+using namespace std;
+template <typename T, typename V>
+auto split(T st, V delim) {
+    char s[sizeof(st)];
+    strcpy(s, st.c_str()); 
+        char * m; 
+        m = strtok(s, delim);
+        vector<int> vc;
+        while(m != NULL){
+            vc.emplace_back(atoi(m));
+            m = strtok(NULL, delim);
+        }
+        return vc;
+    }
+ll mod = 1e9+7;
+
+int main(){
+    
+    FastIO
+
+    #ifndef ONLINE_JUDGE
+        // for getting input from input.txt
+        freopen("input.txt", "r", stdin);
+        // for writing output to output.txt
+        freopen("output.txt", "w", stdout);
+    #endif
+
+   ll x,y,z; cin >> x >> y >> z;
+
+   ll ans1 = x/z + y/z;
+   ll ans2 = 0;
+   ll m = x%z;
+   ll n = y%z;
+
+   ll m_r = z - m;
+   ll n_r = z - n;
+
+   if((n_r <= m_r) && m >= n_r) ans2 = n_r; 
+   else if( (m_r <= n_r) && n >= m_r) ans2 = m_r;
+
+   if(ans2) ans1+=1;
+
+   cout << ans1 << " " << ans2 << endl;
+
+    return 0;
+}
